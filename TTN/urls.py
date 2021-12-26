aa046d181e3d8f68f29f7c3016314780c73813f5
Jashaul <djashaul@gmail.com>
2021-12-26 00:35:37 -0500

    code cleanup

--- TTN/urls.py
+++ TTN/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name='index'),
-    # path('dashboard/', views.index, name='index'),
     # apis
     path('getTrends/', views.GetTrends.as_view()),
     path('getTweets/', views.GetTweets.as_view()),
